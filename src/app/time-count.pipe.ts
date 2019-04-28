9e3a2cf660c02c31c47ffc8bc94c8af50cbeec7d
kangogopeter <peterkangogo501@gmail.com>
2019-04-28 21:41:46 +0300

    reducing error using thevisual studio code problems and terminal but still not functioning even in time-count-pipe.ts

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -7,7 +7,7 @@ export class TimeCountPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     const now: `new Time` = new Time();
-    const nowWithNoDate: any = new Time (now.getFullDay(),  now.getTime());
+    const nowWithNoDate: any = new Time(now.getFullDay(),  now.getTime());
     const timeIntake = Math.abs( nowWithNoDate - value );
     const secondsInADay = 86400;
     const timeDifference = timeIntake * 0.0001;
