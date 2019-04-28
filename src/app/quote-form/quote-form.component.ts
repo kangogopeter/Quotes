717863b7a78557d11b951bf0fc4f3e07802bfb36
kangogopeter <peterkangogo501@gmail.com>
2019-04-28 12:33:16 +0300

    assigned new data

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
+import { Quote } from './quote-form.component';
+import { Time } from './quote-form.component.spec';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
-newQoute = new Quote(0,"",""new Time());
-@Output() addQuote=new EventEmitter<Quote>();
+newQuote = new Quote(0, '', '', new Time());
+@Output() addQuote = new EventEmitter<Quote>();
 
-submitGoal(){
+submitGoal() {
   this.addQuote.emit(this.newQuote);
 }
   constructor() { }
