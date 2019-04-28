62eac7e07744463ac62da4251a1219b57c2a6f6d
kangogopeter <peterkangogo501@gmail.com>
2019-04-28 21:20:56 +0300

    changed file for the component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,12 @@ export class AppComponent {
  bill = 'quotes';
 
  quotes = [
-  new Quote(1, 'My Fear Is Not Death Itself,But A Death Without A Meaning', 'Who Are You', new Time(12, 25)),
-  new Quote(2, 'Seat Back', 'I Slept With A Dog', new Time(09, 15)),
-  new Quote(3, 'Hate Bootcamp', 'Love Coding But Not Bootcamp', new Time(10, 20)),
-  new Quote(4, 'Ingore Pet But Criminate Peter', 'Love Or Hate He Still Rocks', new Time(13, 40)),
-  new Quote(5, 'Steal The Steel', 'Javascript Almost Fucked Me', new Time(14, 59)),
-  new Quote(6, 'Stalkers Are The Worst', 'I Hate You', new Time(16, 12)),
+  new Quote(1, `My Fear Is Not Death Itself,But A Death Without A Meaning`, `Who Are You`, new Time(12, 25)),
+  new Quote(2, `Seat Back`, `I Slept With A Dog`, new Time(09, 15)),
+  new Quote(3, `Hate Bootcamp`, `Love Coding But Not Bootcamp`, new Time(10, 20)),
+  new Quote(4, `Ingore Pet But Criminate Peter`, `Love Or Hate He Still Rocks`, new Time(13, 40)),
+  new Quote(5, `Steal The Steel`, `Javascript Almost Fucked Me`, new Time(14, 59)),
+  new Quote(6, `Stalkers Are The Worst`, `I Hate You`, new Time(16, 12)),
 
 ];
 }
