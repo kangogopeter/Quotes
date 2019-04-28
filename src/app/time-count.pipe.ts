b152479f1e6deb0fa3110af42126c77f7d395c20
kangogopeter <peterkangogo501@gmail.com>
2019-04-28 21:25:06 +0300

    use of const instead of let in time-count.pipe.ts

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -1,12 +1,11 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
+import { Pipe, Time, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'timeCount'
 })
 export class TimeCountPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    const now: 'new Time' = new Time();
+    const now: `new Time` = new Time();
     const nowWithNoDate: any = new Time(now.getFullDay(),  now.getTime());
     const timeIntake = Math.abs( nowWithNoDate - value );
     const secondsInADay = 86400;
