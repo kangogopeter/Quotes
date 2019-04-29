8531694b8d9bdacd726619597a3929d3ad995995
kangogopeter <peterkangogo501@gmail.com>
2019-04-29 14:33:04 +0300

    my commits on error

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
 public showDescription: boolean;
-public showSubmit:boolean;
+public showSubmit: boolean;
 constructor(public id: number, public name: string, public description: string ) {
   this.showDescription = false;
   this.showSubmit = false;
